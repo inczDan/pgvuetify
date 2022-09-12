48f0b849455da0b351b4d51acb65faa77539d54a
inczDan <daniel.alvmatos@gmail.com>
2022-09-12 15:32:11 -0300

    criando pagina de registro, precisa ajeitar display

--- src/views/RegistroView.vue
+++ src/views/RegistroView.vue
@@ -1,10 +1,58 @@
 <template>
-  <div>test</div>
+  <v-form v-model="valid">
+    <v-container>
+      <v-row>
+        <v-col cols="12" md="4">
+          <v-text-field
+            v-model="Usuario"
+            :rules="regrasUser"
+            :counter="10"
+            label="Nome de usuario"
+            required
+          ></v-text-field>
+        </v-col>
+        <v-col cols="12" md="4">
+          <v-text-field
+            v-model="email"
+            :rules="emailRules"
+            label="E-mail"
+            required
+          ></v-text-field>
+        </v-col>
+
+        <v-col cols="12" md="4">
+          <v-text-field
+            type="password"
+            v-model="Senha"
+            :rules="passwordRules"
+            :counter="10"
+            label="Senha"
+            required
+          ></v-text-field>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
 </template>
 
 <script>
 export default {
-  data() {},
-  methods: {},
+  data: () => ({
+    valid: false,
+    Usuario: "",
+    regrasUser: [
+      (v) => !!v || "É obrigatório ter um nome de usuario",
+      (v) => v.length <= 10 || "O nome não pode ultrapassar 10 caracteres!",
+    ],
+    email: "",
+    emailRules: [
+      (v) => !!v || "É obrigatório ter um E-mail",
+      (v) => /.+@.+/.test(v) || "O E-mail não é valido",
+    ],
+    Senha: "",
+    passwordRules: [
+      (v) => v.length >= 8 || "A senha precisa conter entre 8 e 10 caracteres",
+    ],
+  }),
 };
 </script>
