68314a5ae5054ef87fa7f2127464a9ef5b1981a5
inczDan <daniel.alvmatos@gmail.com>
2022-09-13 11:42:44 -0300

    ajustes da tela de login

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,54 +1,57 @@
 <template>
   <div>
     <v-parallax
+      height="1010"
       dark
       src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
     >
       <v-row align="center" justify="center">
         <v-col class="text-center" cols="12">
-          <h1 class="text-h4 font-weight-thin mb-4">To Do list</h1>
-          <h4 class="subheading">Guarde, Realize, Seja!</h4>
+          <h1 class="text-h2 font-weight-thin mb-4">To Do list</h1>
+          <h4 class="text-h5">Guarde, Realize, Seja!</h4>
+          <div class="text-center">
+            <h1 class="text-center msg-login">Bem-vindo(a)!</h1>
+            <v-icon color="grey lighten-1" large> mdi-account </v-icon>
+          </div>
+
+          <v-container>
+            <v-row no-gutters class="caixa-login">
+              <v-col cols="1" sm="4" md="4">
+                <v-card class="pa-2" outlined tile
+                  ><v-text-field
+                    label="Usuario"
+                    placeholder="Insira seu usuario"
+                  />
+                </v-card>
+              </v-col>
+              <v-col cols="6" md="4">
+                <v-card class="pa-2" outlined tile
+                  ><v-text-field
+                    type="password"
+                    label="Senha"
+                    placeholder="Insira sua senha"
+                  />
+                </v-card>
+              </v-col>
+            </v-row>
+            <v-row align="center" justify="space-around">
+              <v-layout justify-center>
+                <v-btn class="btn-login" color="primary" @click="vaiParaInicio">
+                  Login
+                </v-btn>
+                <v-btn
+                  class="justify-center"
+                  color="primary"
+                  @click="vaiParaRegistro"
+                >
+                  Registrar-se
+                </v-btn>
+              </v-layout>
+            </v-row>
+          </v-container>
         </v-col>
       </v-row>
     </v-parallax>
-
-    <div class="text-center">
-      <h1 class="text-center">Bem-vindo(a)!</h1>
-      <v-icon color="grey lighten-1" large> mdi-account </v-icon>
-    </div>
-
-    <v-container class="grey lighten-5">
-      <v-row no-gutters class="caixa-login">
-        <v-col cols="1" sm="4" md="4">
-          <v-card class="pa-2" outlined tile
-            ><v-text-field label="Usuario" placeholder="Insira seu usuario" />
-          </v-card>
-        </v-col>
-        <v-col cols="6" md="4">
-          <v-card class="pa-2" outlined tile
-            ><v-text-field
-              type="password"
-              label="Senha"
-              placeholder="Insira sua senha"
-            />
-          </v-card>
-        </v-col>
-      </v-row>
-      <v-row align="center" justify="space-around">
-        <v-layout justify-center>
-          <v-btn class="btn-login" color="primary" @click="vaiParaInicio">
-            Login
-          </v-btn>
-          <v-btn
-            class="justify-center"
-            color="primary"
-            @click="vaiParaRegistro"
-          >
-            Registrar-se
-          </v-btn>
-        </v-layout>
-      </v-row>
-    </v-container>
   </div>
 </template>
 
@@ -79,5 +82,8 @@ export default {
 }
 .btn-login {
   margin-right: 13.5%;
+}
+.msg-login {
+  margin-top: 10%;
 }
 </style>
